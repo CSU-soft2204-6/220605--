3deeb5c769b764ca3d66837bb1a8f6853de05a14
quensol <60693412+quensol@users.noreply.github.com>
2022-12-13 10:58:03 +0800

    Update 多态性.cpp

--- 多态性.cpp
+++ 多态性.cpp
@@ -1,43 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Complex {
+class Complex{
 public:
-	double real;
-	double image;
-public:
-	Complex()
-	{
-		real = 0;
-		image = 0;
-	}
-	Complex(double r, double i)
-	{
-		real = r;
-		image = i;
-	}
-	void display()
-	{
-		cout << real << '+' << image << 'i' << endl;
-	}
-	friend Complex operator +(Complex A, Complex B)
-	{
-		return Complex(A.real + B.real, A.image + B.image);
-	}
-	friend Complex operator ++(Complex A)
-	{
-		return Complex(A.real + 1, A.image);
-	}
+    double real;
+    double image;
+public:Complex() {
+    real = 0;
+    image = 0;
+}
+      Complex(double t_real,double t_image) {
+          this->real = t_real;
+          this->image = t_image;
+      }
+      void disply() {
+          cout << "real=" << this->real << ";image=" << this->image << endl;
+      }
+      friend Complex operator +(Complex A, Complex B) {
+          return Complex(A.real + B.real, A.image + B.image);
+      }
+      friend Complex operator ++(Complex& A) {
+          return Complex(++A.real,A.image);
+      }
+      friend Complex operator ++(Complex& A ,int) {
+          return Complex(++A.real, A.image);
+      }
 };
-int main()
+int main(double sid1,double sid2,double sid3)
 {
-	Complex c1(5.4, 1.1);
-	Complex c2(5.8, -1.9);
-	Complex c3 = c1 + c2;
-	c1.display();
-	c2.display();
-	c3.display();
-	c3++;
-	c3.display();
-	system("pause");
-	return 0;
+    Complex comp1;
+    comp1.real=1.0;
+    comp1.image = 1.0;
+    comp1.disply();
+
+    Complex comp2(2.0,2.0);
+    comp2.disply();
+    Complex comp3 = comp1 + comp2;
+    comp3.disply();
+    Complex comp4 = comp3++;
+    
+    comp4.disply();
+    system("pause");
 }
